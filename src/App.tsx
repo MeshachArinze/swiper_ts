8f8bedf0c487886b8c958d9b0f6974606895678c
MeshachArinze <meshacharinze@gmail.com>
2023-03-14 01:07:01 +0100

    third commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import Swiper from "./components/Swiper"
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const items = [
+    {
+      imageSrc: "./assets/food1.jpg",
+      imageAlt: "cup cake",
+    },
+    {
+      imageSrc: "./assets/food2.jpg",
+      imageAlt: "foodie",
+    },
+    {
+      imageSrc: "./assets/food3.jpg",
+      imageAlt: "cake",
+    },
+    {
+      imageSrc: "./assets/food4.jpg",
+      imageAlt: "spag",
+    },
+    {
+      imageSrc: "./assets/food5.jpg",
+      imageAlt: "macaronni",
+    },
+  ];
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Swiper items={[]} />
     </div>
   )
 }
--- src/components/Swiper.tsx
+++ src/components/Swiper.tsx
@@ -1,12 +1,16 @@
-import React from 'react'
-import  "./Swiper.css";
+import React from "react";
+import { SwiperItemType } from "../types";
+import "./Swiper.css";
+import SwiperItem from "./SwiperItem";
 
-export default function Swiper():JSX.Element {
+export type Props = {
+  items: Array<SwiperItemType>;
+};
+
+export default function Swiper({ items }: Props): JSX.Element {
   return (
     <div className="Swiper-container">
-        <ul className='Swiper_list'>
-
-        </ul>
+      <ul className="Swiper_list">{items.map((item, idx) => (<SwiperItem key={idx} {...item} />))}</ul>
     </div>
-  )
+  );
 }
